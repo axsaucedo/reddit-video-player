e6b6be36dd9bc121d608ddaf62176b3054fb3d6f
Alejandro Saucedo <axsauze@gmail.com>
2017-02-23 22:49:46 +0000

    deploy

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,4 +1,15 @@
+#!/bin/sh
 
+# stop on error
+set -e 
+
+# Building code
+npm run-script build
+
+# Replacing all the necessary stuff
+sed -i '' 's|<base href="/">|<base href="/reddit-video-crawler/">|g' build/index.html
+
+# Deploying to gh-pages
 git branch -D gh-pages
 git checkout -b gh-pages
 git add $1 --force
