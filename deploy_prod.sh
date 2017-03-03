4adffe87eb1f20a91e68b3028204455f6b05f457
Alejandro Saucedo <axsauze@gmail.com>
2017-03-03 21:37:14 +0000

    changed remote git

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -8,11 +8,11 @@ npm run-script build
 
 # Replacing all the necessary stuff
 # Basically because github pages uses Jekyll, we need to make sure we have prepended /reddit-video-crawler/ so it finds all files
-sed -i '' 's|<base href="/">|<base href="/reddit-video-crawler/">|g' build/index.html
-find build/assets -type f -name "*.js" -print0 | xargs -0 sed -i '' 's|navbar.html|/reddit-video-crawler/navbar.html|g'
-find build/assets -type f -name "*.js" -print0 | xargs -0 sed -i '' 's|redditvideo.html|/reddit-video-crawler/redditvideo.html|g'
-find build/assets -type f -name "*.js" -print0 | xargs -0 sed -i '' 's|app.html|/reddit-video-crawler/app.html|g'
-find build/assets -type f -name "*.js" -print0 | xargs -0 sed -i '' 's|home.css|/reddit-video-crawler/home.css|g'
+sed -i '' 's|<base href="/">|<base href="/reddit-video-player/">|g' build/index.html
+find build/assets -type f -name "*.js" -print0 | xargs -0 sed -i '' 's|navbar.html|/reddit-video-player/navbar.html|g'
+find build/assets -type f -name "*.js" -print0 | xargs -0 sed -i '' 's|redditvideo.html|/reddit-video-player/redditvideo.html|g'
+find build/assets -type f -name "*.js" -print0 | xargs -0 sed -i '' 's|app.html|/reddit-video-player/app.html|g'
+find build/assets -type f -name "*.js" -print0 | xargs -0 sed -i '' 's|home.css|/reddit-video-player/home.css|g'
 
 # Deploying to gh-pages
 git branch -D gh-pages || true
